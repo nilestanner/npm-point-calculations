5a36a7a070e5a77057fcdcc86754edbdb6ad797d
Niles Tanner <niles.tanner@gmail.com>
2019-04-14 17:44:54 -0500

    added base fomulas

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Point } from './models/point';
+
+export const distance = (startPoint: Point, endPoint: Point): number => {
+    return Math.sqrt( Math.abs( Math.pow(startPoint.x - endPoint.x, 2) + Math.pow(startPoint.y - endPoint.y, 2)));
+}
+export const angle = (startPoint: Point, endPoint: Point): number => {
+    return Math.atan2((startPoint.y - endPoint.y), (startPoint.x - endPoint.x));
+}
+
+export const rotatePoint = (rotatingPoint: Point, centerPoint: Point, angle: number): Point => {
+	const cos: number = Math.cos(angle);
+	const sin: number = Math.sin(angle);
+	const nx: number = Math.round((cos * (centerPoint.x - rotatingPoint.x)) + (sin * (centerPoint.y - rotatingPoint.y)) + rotatingPoint.x);
+	const ny: number = Math.round((cos * (centerPoint.y - rotatingPoint.y)) - (sin * (centerPoint.x - rotatingPoint.x)) + rotatingPoint.y);
+	return { x:nx, y:ny };
+}
+
+export const translatePoint = (point: Point, distance: number, angle: number = 0): Point => {
+    return {
+		x:(Math.cos(angle) * distance) + point.x,
+		y:(Math.sin(angle) * distance) + point.y
+	};
+}
+
+export const findPointOnLine = (startPoint: Point, endPoint: Point, distanceFromStart: number): Point => {
+    const fullDistance: number = distance(startPoint, endPoint);
+	const ratio: number = distanceFromStart/fullDistance;
+	return {
+		x:((1-ratio) * startPoint.x + (ratio) * endPoint.x),
+		y:((1-ratio) * startPoint.y + (ratio) * endPoint.y)
+	};
+}
